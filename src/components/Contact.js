66bd8002603ec3ba2ecfc997c46b834693232c15
SAMMBENDE <51370083+SAMMBENDE@users.noreply.github.com>
2020-07-30 21:50:22 +0200

    'router'

---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div className="container-contact">Contact</div>
+    );
+  }
+}
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container-home">Home</div>
+    );
+  }
+}
+export default Home;
